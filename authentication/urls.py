c50e8afa8221b52dcecb26a054a27a7296213e11
Hussein Kadweka <developersjuniors@gmail.com>
2019-12-24 15:03:58 +0300

    defined firebase authentication and create templates

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    url(r'^$', views.home,name='home'),
+    url(r'^home/', views.home,name='home'),
+    url(r'^$',views.signIn),
+    url(r'^postsign/',views.postsign),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render,redirect,get_list_or_404
+import pyrebase
+
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+
+    config = {
+    'apiKey': "AIzaSyC0Ba-3qLhzi8hwxVGYRIM2c1WVpmanmzc",
+    'authDomain': "cpanel-5e873.firebaseapp.com",
+    'databaseURL': "https://cpanel-5e873.firebaseio.com",
+    'projectId': "django-authentication-9552d",
+    'storageBucket': "http://django-authentication-9552d.appspot.com/",
+    'messagingSenderId': "624344477485"
+    }
+    firebase = pyrebase.initialize_app(config)
+    auth = firebase.auth()
+def signIn(request):
+    return render(request, "signIn.html")
+
+def postsign(request):
+    email=request.POST.get('email')
+    passw = request.POST.get("pass")
+    try:
+        user = auth.sign_in_with_email_and_password(email,passw)
+    except:
+        message = "CREDENTIALS ERROR!"
+    return render(request,"signIn.html",{"msg":message})
+    print(user)
+    return render(request, "welcome.html",{"e":email})
