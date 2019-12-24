a653936669dd5d23141050c4af0eafb9bcfc29d9
Hussein Kadweka <developersjuniors@gmail.com>
2019-12-24 13:42:49 +0300

    create project and app in addition to installing of django and virtual environment

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/FIRE-BASE-AUTHENTICATION-DJANGO/firebase_auth/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
